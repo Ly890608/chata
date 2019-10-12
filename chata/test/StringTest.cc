80e24fa1f9e8cb7d6146492c2fd0718b1d3663db
ly <ly890608@gmail.com>
2019-10-12 22:58:41 +0800

    Add Unit Test

--- chata/test/StringTest.cc
+++ chata/test/StringTest.cc
@@ -3,7 +3,18 @@
 
 TEST(String, Split)
 {
-    auto sp = chata::String::Split("abc.edf", ".");
+    auto sp = chata::String::Split(".abc.edf.123456..", ".");
+    EXPECT_EQ(sp.size(), 3);
     EXPECT_EQ(sp[0], "abc");
     EXPECT_EQ(sp[1], "edf");
+    EXPECT_EQ(sp[2], "123456");
+}
+
+TEST(String, Split_MultyDelims)
+{
+    auto sp = chata::String::Split(".abc+edf/.123456..", "./+");
+    EXPECT_EQ(sp.size(), 3);
+    EXPECT_EQ(sp[0], "abc");
+    EXPECT_EQ(sp[1], "edf");
+    EXPECT_EQ(sp[2], "123456");
 }
